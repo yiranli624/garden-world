19323947eb46df67969eb57546e093c65f9e8ffb
Yiran Li <yiran.li@hotmail.com>
2025-02-01 22:38:10 -0700

    [Add more handler for the rest of csv files]

--- scripts/loadCsvFile.ts
+++ scripts/loadCsvFile.ts
@@ -8,6 +8,45 @@ async function getCsvString(filePath: string) {
   return await fs.readFile(filePath, "utf8");
 }
 
+function changeStringToArray(str: string) {
+  if (str.trim() === "") {
+    return [];
+  }
+  return str
+    .trim()
+    .split(",")
+    .filter((eachStr) => eachStr !== "");
+}
+function changeStringToNumber(options: {
+  str: string;
+  allowUndefined: false;
+  replaceDecimal?: boolean;
+}): number;
+
+function changeStringToNumber(options: {
+  str: string;
+  allowUndefined: true;
+  replaceDecimal?: boolean;
+}): number | undefined;
+
+function changeStringToNumber({
+  str,
+  allowUndefined,
+  replaceDecimal = false
+}: {
+  str: string;
+  allowUndefined: boolean;
+  replaceDecimal?: boolean;
+}) {
+  if (str.trim() === "" && allowUndefined) {
+    return undefined;
+  }
+  if (str) {
+    return replaceDecimal ? Number(str.replace(".", "")) : Number(str);
+  }
+  throw new Error(`The ${str} should not be undefined`);
+}
+
 async function handleCategory() {
   const csvString = await getCsvString(
     "/Users/elle/Elle/code/garden-world/category.csv"
@@ -53,8 +92,9 @@ async function handleCategory() {
         .insertInto("category")
         .values(udpatedCategory)
         .returning(["id", "slug"])
-        .executeTakeFirst();
-      categoryDbRow && categoryParialDbCols.push(categoryDbRow);
+        .executeTakeFirstOrThrow();
+
+      categoryParialDbCols.push(categoryDbRow);
     }
   });
 }
@@ -90,6 +130,133 @@ async function handleAnnouncement() {
   }
 }
 
+async function handleProduct() {
+  const csvString = await getCsvString(
+    "/Users/elle/Elle/code/garden-world/product.csv"
+  );
+  const productCsvRows: {
+    slug: string;
+    label: string;
+    imagesUrls: string;
+    videoIds: string;
+    originalPrice: string;
+    salesPrice: string;
+    categorySlug: string;
+    sku: string;
+    stockAmount: string;
+    amountPerPack: string;
+    collection: string;
+    englishDescription: string;
+    chineseDescription: string;
+    instructionImageUrls: string;
+    additionalInfo: string;
+  }[] = [];
+
+  await new Promise<void>((resolve) => {
+    const productData = parse({ headers: true })
+      .on("error", (error) => {
+        throw error;
+      })
+      .on("data", (row) => productCsvRows.push(row))
+      .on("end", (rowCount: number) => {
+        console.log(`Parsed ${rowCount} rows`);
+        resolve();
+      });
+
+    productData.write(csvString);
+    productData.end();
+  });
+
+  await db.transaction().execute(async (txn) => {
+    for (const productRow of productCsvRows) {
+      const reformAdditionalInfo = productRow.additionalInfo
+        .trim()
+        .split(";")
+        .reduce<Record<string, string>>((reformedInfo, originalInfo) => {
+          const infoSlice = originalInfo.split(":");
+          reformedInfo[infoSlice[0].trim()] = infoSlice[1].trim();
+          return reformedInfo;
+        }, {});
+
+      const { id: categoryId } = await txn
+        .selectFrom("category")
+        .select("id")
+        .where("slug", "=", productRow.categorySlug)
+        .executeTakeFirstOrThrow();
+
+      const collectionArray = changeStringToArray(productRow.collection);
+
+      const collections =
+        collectionArray.length > 0
+          ? await txn
+              .selectFrom("category")
+              .selectAll()
+              .where("slug", "in", collectionArray)
+              .execute()
+          : [];
+
+      if (collectionArray.length !== collections.length) {
+        // console.log(collectionArray.length, collections.length);
+        throw new Error(
+          `Should have all collections but not, product.collection is ${collectionArray.join(
+            ","
+          )}, and collections are ${collections.join(",")}`
+        );
+      }
+
+      const udpatedProduct = {
+        ...productRow,
+        imagesUrls: changeStringToArray(productRow.imagesUrls),
+        videoIds: changeStringToArray(productRow.videoIds),
+        categoryId,
+        originalPrice: changeStringToNumber({
+          str: productRow.originalPrice,
+          allowUndefined: false,
+          replaceDecimal: true
+        }),
+        salesPrice: changeStringToNumber({
+          str: productRow.salesPrice,
+          allowUndefined: true,
+          replaceDecimal: true
+        }),
+        stockAmount: changeStringToNumber({
+          str: productRow.stockAmount,
+          allowUndefined: false
+        }),
+        amountPerPack: changeStringToNumber({
+          str: productRow.amountPerPack,
+          allowUndefined: true
+        }),
+        instructionImageUrls: changeStringToArray(
+          productRow.instructionImageUrls
+        ),
+        additionalInfo: reformAdditionalInfo
+      };
+      // @ts-expect-error
+      delete udpatedProduct.categorySlug;
+      // @ts-expect-error
+      delete udpatedProduct.collection;
+
+      const productDbRow = await txn
+        .insertInto("product")
+        .values(udpatedProduct)
+        .returning(["id", "slug"])
+        .executeTakeFirstOrThrow();
+
+      if (collections.length > 0) {
+        const productCollectionRows = collections.map((collection) => ({
+          productId: productDbRow.id,
+          collectionId: collection.id
+        }));
+        await txn
+          .insertInto("productCollection")
+          .values(productCollectionRows)
+          .execute();
+      }
+    }
+  });
+}
+
 async function main() {
   const { file, table } = await yargs(hideBin(process.argv))
     .option("f", {
@@ -105,15 +272,14 @@ async function main() {
       type: "string"
     }).argv;
 
-  switch (table) {
-    case "category":
+  switch (file) {
+    case "category.csv":
       await handleCategory();
       break;
-    case "product":
+    case "product.csv":
+      await handleProduct();
       break;
-    case "productCollection":
-      break;
-    case "announcement":
+    case "announcement.csv":
       await handleAnnouncement();
       break;
     default:
