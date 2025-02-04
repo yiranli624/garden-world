1f2af8fadbb18f0010c2470fdc185299a7cbba0f
Yiran Li <yiran.li@hotmail.com>
2025-02-03 22:27:33 -0700

    [Add category query]

---
+++ src/queries/categoryQueries.ts
@@ -0,0 +1,27 @@
+import "server-only";
+import { db } from "@/database";
+import { Category } from "@/types";
+
+export type FullCategory = Category & {
+  parentCategory?: Category;
+};
+
+export async function getCategories(): Promise<FullCategory[]> {
+  const rawCategories = await db.selectFrom("category").selectAll().execute();
+
+  const flatCategories = await Promise.all(
+    rawCategories.map(async (category) => {
+      let parentCategory;
+      if (category.parentId) {
+        parentCategory = await db
+          .selectFrom("category")
+          .selectAll()
+          .where("category.id", "=", category.parentId)
+          .executeTakeFirstOrThrow();
+      }
+      return { ...category, parentCategory: parentCategory };
+    })
+  );
+
+  return flatCategories;
+}
